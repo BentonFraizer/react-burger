40b45a75e6d6054906f6095f4c1e88bb8a292305
yuri_lopukhin <fol2203012@yandex.ru>
2024-03-16 14:16:23 +0300

    feat(ingredients): add layout for ingredients-group and several ingredient-item. Add some styles

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
-import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import { CurrencyIcon, Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import s from './burger-ingredients.module.css';
+import { data } from '../../utils/data';
 
 function BurgerIngredients() {
   const [current, setCurrent] = useState('buns');
+  const firstBurger = data[0];
+
   return (
     <>
       <div className={s['burger-ingredients']}>
@@ -25,19 +28,52 @@ function BurgerIngredients() {
           {/* Код ниже разнести по разным файлам согласно скриншота из видео */}
           <div className={s['ingredients-group']}>
             <div className={s['ingredients-group__title']}>
-              <p className="text text_type_main-medium">
+              <p className='text text_type_main-medium'>
                 Булки
               </p>
             </div>
             <div className={s['ingredient-item__container']}>
               <div className={s['ingredient-item']}>
-                <div className={s['ingredient-item__img']}></div>
-                <div className={s['ingredient-item__price']}>
-                  <div className={s['price-value']}></div>
-                  <div className={s['price-icon']}></div>
+                <div className={s['ingredient-item__img']}>
+                  <img src={firstBurger.image} alt={firstBurger.name} />
                 </div>
-                <div className={s['ingredient-item__name']}></div>
-                <div className='ingredient-item__counter'></div>
+                <div className={s['ingredient-item__price']}>
+                  <div className={s['price-value']}>
+                    <p className='text text_type_digits-default'>{firstBurger.price}</p>
+                  </div>
+                  <div className={s['price-icon']}>
+                    <CurrencyIcon type='primary' />
+                  </div>
+                </div>
+                <div className={s['ingredient-item__name']}>
+                  <p className='text text_type_main-default'>
+                    {firstBurger.name}
+                  </p>
+                </div>
+                <div className='ingredient-item__counter'>
+                  <p className='text text_type_digits-default'>1</p>
+                </div>
+              </div>
+              <div className={s['ingredient-item']}>
+                <div className={s['ingredient-item__img']}>
+                  <img src={firstBurger.image} alt={firstBurger.name} />
+                </div>
+                <div className={s['ingredient-item__price']}>
+                  <div className={s['price-value']}>
+                    <p className='text text_type_digits-default'>{firstBurger.price}</p>
+                  </div>
+                  <div className={s['price-icon']}>
+                    <CurrencyIcon type='primary' />
+                  </div>
+                </div>
+                <div className={s['ingredient-item__name']}>
+                  <p className='text text_type_main-default'>
+                    {firstBurger.name}
+                  </p>
+                </div>
+                <div className='ingredient-item__counter'>
+                  <p className='text text_type_digits-default'>1</p>
+                </div>
               </div>
             </div>
           </div>

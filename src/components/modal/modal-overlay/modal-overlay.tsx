ecfbf13a94274322e3d227816c124d17a5f58a23
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-03-17 23:53:17 +0300

    feat(modal): implemented modal and modal-overlay components

---
+++ src/components/modal/modal-overlay/modal-overlay.tsx
@@ -0,0 +1,9 @@
+import s from './modal-overlay.module.css';
+
+function ModalOverlay() {
+  return (
+    <div className={s['modal-overlay']} />
+  );
+}
+
+export default ModalOverlay;
---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,34 @@
+import React, { JSX } from 'react';
+import s from './modal.module.css';
+import xmark from '../../images/icons/xmark.png';
+import ModalOverlay from './modal-overlay/modal-overlay';
+
+type ModalProps = {
+  title?: string;
+  children: React.ReactNode;
+};
+
+function Modal({ title, children }: ModalProps): JSX.Element {
+  return (
+    <div className={s.modal}>
+      <div className={s['modal-wrapper']}>
+        <ModalOverlay />
+        <div className={s['modal-content']}>
+          <div className={`${s['modal-header']} mb-4`}>
+            <p className='text text_type_main-large'>
+              {title}
+            </p>
+            <div className={s['close-btn']}>
+              <img src={xmark} alt='крестик закрытия модального окна' />
+            </div>
+          </div>
+          <div className={s['modal-content__wrapper']}>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;

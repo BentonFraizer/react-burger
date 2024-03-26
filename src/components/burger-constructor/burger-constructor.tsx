45cd730807ab503d4f1a0932a8a86ae4307a4f72
yuri_lopukhin <fol2203012@yandex.ru>
2024-03-26 13:30:10 +0300

    feat(constructor): add random amount of ingredients in burger-constructor component

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,15 +1,35 @@
 import { Button, ConstructorElement, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import s from './burger-constructor.module.css';
 import Modal from '../modal/modal';
 import OrderDetails from '../order-details/order-details';
 import { useModal } from '../../hooks/useModal';
 import { IngredientsContext } from '../../services/ingredientsContext';
+import { generateRandomIngredients } from '../../utils/utils';
+import { Ingredient } from '../../types';
 
 function BurgerConstructor() {
   const { data } = useContext(IngredientsContext);
-  const firstBurger = data[0];
-  const mains = data.filter((ingredient) => ingredient.type === 'main');
+  // Код ниже необходим для генерации случайного количества ингредиентов между закреплёнными элементами типа bun
+  const [randomIngredients, setRandomIngredients] = useState<Ingredient[]>([]);
+  const [mains, setMains] = useState<Ingredient[]>([]);
+
+  useEffect(() => {
+    if (data.length !== 0) {
+      const random = generateRandomIngredients(data, 2, data.length - 1);
+      setRandomIngredients(random);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (randomIngredients.length !== 0) {
+      const exceptBunIngredients = randomIngredients.filter((ingredient) => ingredient.type !== 'bun');
+      setMains(exceptBunIngredients);
+    }
+  }, [randomIngredients]);
+
+  const bun = randomIngredients[0];
+
   const { isModalOpened, openModal, closeModal } = useModal();
 
   return (
@@ -17,9 +37,9 @@ function BurgerConstructor() {
       <ConstructorElement
         type='top'
         isLocked={true}
-        text='Краторная булка N-200i (верх)'
-        price={200}
-        thumbnail={firstBurger?.image}
+        text={bun?.name}
+        price={bun?.price}
+        thumbnail={bun?.image}
         extraClass={'mb-4'}
       />
       <ul className={s['constructor-elements__wrapper']}>
@@ -37,9 +57,9 @@ function BurgerConstructor() {
       <ConstructorElement
         type='bottom'
         isLocked={true}
-        text='Краторная булка N-200i (низ)'
-        price={200}
-        thumbnail={firstBurger?.image}
+        text={bun?.name}
+        price={bun?.price}
+        thumbnail={bun?.image}
         extraClass={'mt-4 mb-10'}
       />
       <div className={s['preorder-info']}>
@@ -56,8 +76,8 @@ function BurgerConstructor() {
         </Button>
       </div>
       {isModalOpened && <Modal onClose={closeModal} isModalOpen={isModalOpened}>
-          <OrderDetails />
-        </Modal>}
+        <OrderDetails />
+      </Modal>}
     </div>
   );
 }

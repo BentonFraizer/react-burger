99e1faa415ffd799dfc198907ca8284e1d3368c1
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-04-09 20:57:47 +0300

    feat(router): add routing into the project. Add main page component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,20 +1,15 @@
 import React, { JSX } from 'react';
-import s from './app.module.css';
-import AppHeader from '../app-header/app-header';
-import BurgerIngredients from '../burger-ingredients/burger-ingredients';
-import BurgerConstructor from '../burger-constructor/burger-constructor';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../consts';
+import { MainPage } from '../../pages/main-page/main-page';
 
 function App():JSX.Element {
   return (
-    <div className={s.app}>
-      <AppHeader />
-      <main className={s.main}>
-        <section className={s.section}>
-          <BurgerIngredients />
-          <BurgerConstructor />
-        </section>
-      </main>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,14 @@
 export const APIRoute = {
   ingredients: 'ingredients',
-  orders: 'order'
+  orders: 'order',
+};
+
+export const AppRoute = {
+  Main: '/',
+
+  // Catalog: '/catalog/page_:pageNumber',
+  // Product: '/product/:id',
+  // Basket: '/basket',
+  // Offline: '/offline',
+  // FailedOrder: '/failed-order',
 };

63ea494858f210fb916bd22ae797cea03d20dedb
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-05-14 20:08:55 +0300

    feat(utils): make getIngredientImages utils function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,16 @@
+import { Ingredient, Order } from '../types';
+
 // Функция определения нажатия клавиши Escape
 export const isEscKeyPressed = (evt: KeyboardEvent) => evt.key === 'Escape' || evt.key === 'Esc';
+
+// Функция получения массива ссылок на изображения ингредиентов
+export const getIngredientImages = (allIngredients: Ingredient[], order: Order): string[] => {
+  const ingredientImages: string[] = [];
+  order.ingredients.forEach((ingredientId) => {
+    const currentIngredient = allIngredients.find((ingredient) => ingredient._id === ingredientId);
+    if (currentIngredient) {
+      ingredientImages.push(currentIngredient.image);
+    }
+  });
+  return ingredientImages;
+};

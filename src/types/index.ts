0ed57731948ed5d05d2be44a65d8c4a9e478f28f
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-05-14 20:06:30 +0300

    feat(types): add Order type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export type { default as Ingredient } from './ingredient';
 export type { default as UniqueIdIngredient } from './uniqueIdIngredient';
 export type { default as User } from './user';
 export type { default as Register } from './register';
+export type { default as Order } from './order';

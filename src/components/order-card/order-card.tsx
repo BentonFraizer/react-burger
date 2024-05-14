b81b19b4bc7ed39f607ba251727340b54b51fe2a
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-05-14 20:46:59 +0300

    feat(order-card): modify order-card component with mock data. Include it into all-orders-page component

--- src/components/order-card/order-card.tsx
+++ src/components/order-card/order-card.tsx
@@ -1,42 +1,58 @@
 import { JSX } from 'react';
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { CurrencyIcon, FormattedDate } from '@ya.praktikum/react-developer-burger-ui-components';
+import { v4 as uuidv4 } from 'uuid';
 import s from './order-card.module.css';
+import { Ingredient, Order } from '../../types';
+import { useAppSelector } from '../../hooks/hooks';
+import { IngredientPreview } from './ingredient-preview/ingredient-preview';
+import { getIngredientImages, getOrderPrice } from '../../utils/utils';
 
-function OrderCard(): JSX.Element {
+type OrderCardProps = {
+  orderInfo: Order;
+}
+
+const MAX_INGREDIENTS_PREVIEWS_COUNT = 6;
+
+function OrderCard({ orderInfo }: OrderCardProps): JSX.Element {
+  const { ingredients } = useAppSelector((state) => state.ingredients);
+  const { number, createdAt, name } = orderInfo;
+  const images = getIngredientImages(ingredients as Ingredient[], orderInfo).reverse();
+
+  const extraIngredientsCount = images.length >= 6 ? images.length - MAX_INGREDIENTS_PREVIEWS_COUNT : images.length;
+
   return (
     <div className={s.order__card}>
       <div className={s.order__info}>
         <div className='order__number'>
-          <p className='text text_type_digits-default'>#034535</p>
+          <p className='text text_type_digits-default'>#{number}</p>
         </div>
         <div className={s['order__created-date']}>
           <p className='text text_type_main-default'>
-            Сегодня, 16:20
+            <FormattedDate date={new Date(createdAt)} />
           </p>
         </div>
       </div>
       <div className={s.order__title}>
         <p className='text text_type_main-medium'>
-          Death Star Starship Main бургер
+          {name}
         </p>
       </div>
       <div className={s.order__details}>
         <div className={s.ingredients__icons}>
-          <div className={s.ingredient__preview}>
-            <img src='https://code.s3.yandex.net/react/code/meat-02.png' alt='small ingredient' />
-          </div>
-          <div className={s.ingredient__preview}>
-            <img src='https://code.s3.yandex.net/react/code/sp_1.png' alt='small ingredient' />
-          </div>
-          <div className={s.ingredient__preview}>
-            <img src='https://code.s3.yandex.net/react/code/mineral_rings.png' alt='small ingredient' />
-          </div>
-          <div className={s.ingredient__preview}>
-            <img src='https://code.s3.yandex.net/react/code/meat-01.png' alt='small ingredient' />
-          </div>
+          {images.length <= MAX_INGREDIENTS_PREVIEWS_COUNT
+            ? images.map((image) => <IngredientPreview key={uuidv4()} imageSrc={image} />)
+            : images.splice(0, MAX_INGREDIENTS_PREVIEWS_COUNT)
+              .map((image, index) => {
+                if (index === 0) {
+                  return <IngredientPreview key={uuidv4()} imageSrc={image} extraIngredientsCount={extraIngredientsCount}/>;
+                }
+                return <IngredientPreview key={uuidv4()} imageSrc={image} />;
+              })
+          }
         </div>
+
         <div className={s.order__price}>
-          <p className='text text_type_digits-default mr-2'>480</p>
+          <p className='text text_type_digits-default mr-2'>{getOrderPrice(ingredients as Ingredient[], orderInfo)}</p>
           <CurrencyIcon type='primary' />
         </div>
       </div>
--- src/pages/all-orders-page/all-orders-page.tsx
+++ src/pages/all-orders-page/all-orders-page.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import s from './all-orderst-page.module.css';
 import OrderCard from '../../components/order-card/order-card';
+import { Order } from '../../types';
 
 function AllOrdersPage() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [pageInfo, setPageInfo] = useState();
+  const getCardsData = async () => {
+    const response = await fetch('http://localhost:3001/norma.nomoreparties.space/orders/all');
+    return response.json();
+  };
+
+  useEffect(() => {
+    getCardsData().then((data) => {
+      setOrders(data.orders);
+      setPageInfo(data);
+    });
+  }, []);
+
   return (
     <main className={s.main}>
       <section className={s.section}>
@@ -11,7 +26,11 @@ function AllOrdersPage() {
             Лента заказов
           </p>
           <div className={s.cards__wrapper}>
-            <OrderCard/>
+            {
+              orders.map((order) => (
+                <OrderCard key={order._id} orderInfo={order} />
+              ))
+            }
           </div>
         </div>
 

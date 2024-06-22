64262417c91ac975d347120211c5052c22ba3256
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-06-23 00:06:45 +0300

    feat(constructor ingredients): add mock data for tests

---
+++ src/services/reducers/mock/mockForTests.ts
@@ -0,0 +1,49 @@
+import { UniqueIdIngredient } from '../../../types';
+
+export const MOCK_BUN_INGREDIENT: UniqueIdIngredient = {
+  _id: '643d69a5c3f7b9001cfa093d',
+  name: 'Флюоресцентная булка R2-D3',
+  type: 'bun',
+  proteins: 44,
+  fat: 26,
+  carbohydrates: 85,
+  calories: 643,
+  price: 988,
+  image: 'https://code.s3.yandex.net/react/code/bun-01.png',
+  image_mobile: 'https://code.s3.yandex.net/react/code/bun-01-mobile.png',
+  image_large: 'https://code.s3.yandex.net/react/code/bun-01-large.png',
+  __v: 0,
+  uniqueId: '7d2e5fad-a04c-49cf-93a5-1cd7cab1ec2f',
+};
+
+export const MOCK_NOT_BUN_INGREDIENT_1: UniqueIdIngredient = {
+  _id: '643d69a5c3f7b9001cfa0941',
+  name: 'Биокотлета из марсианской Магнолии',
+  type: 'main',
+  proteins: 420,
+  fat: 142,
+  carbohydrates: 242,
+  calories: 4242,
+  price: 424,
+  image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+  image_mobile: 'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+  image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png',
+  __v: 0,
+  uniqueId: 'f5db76dc-602a-4956-913d-fb815d355dde',
+};
+
+export const MOCK_NOT_BUN_INGREDIENT_2: UniqueIdIngredient = {
+  _id: '643d69a5c3f7b9001cfa0943',
+  name: 'Соус фирменный Space Sauce',
+  type: 'sauce',
+  proteins: 50,
+  fat: 22,
+  carbohydrates: 11,
+  calories: 14,
+  price: 80,
+  image: 'https://code.s3.yandex.net/react/code/sauce-04.png',
+  image_mobile: 'https://code.s3.yandex.net/react/code/sauce-04-mobile.png',
+  image_large: 'https://code.s3.yandex.net/react/code/sauce-04-large.png',
+  __v: 0,
+  uniqueId: 'd301a26f-d176-4465-a805-5f98fdc7ed79'
+};
